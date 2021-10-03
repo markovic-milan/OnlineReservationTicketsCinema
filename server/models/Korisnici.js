bd145b1d2b0e1157a35bcd13ae8bb8bc353baac8
Ivona <ivonaymarmat@gmail.com>
2021-10-03 12:59:20 +0200

    Tabela Korisnici

--- server/models/Korisnici.js
+++ server/models/Korisnici.js
@@ -1,10 +1,26 @@
-module.exports = (sequalize, DataTypes) =>{
-
-    const Korisnici = sequalize.define("Korisnici",{
-       
-
-
-    })
-    return Korisnici
-
-}
+module.exports=(sequelize,DataTypes)=> {
+    const Korisnici=sequelize.define("Korisnici", {
+        korisnicko_ime: {
+            type:DataTypes.STRING,
+            allowNull:false,
+            },
+        lozinka: {
+                type:DataTypes.STRING,
+                allowNull:false,
+                },
+        ime: {
+            type:DataTypes.STRING,
+            allowNull:false,
+            },
+        prezime:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            },
+        email:{
+                type:DataTypes.STRING,
+                allowNull:false,
+                },
+            });
+            
+            return Korisnici;
+            };
