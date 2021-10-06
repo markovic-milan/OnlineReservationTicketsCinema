c6316f6237fe911e4d32ce323f8a481dd9bb3c66
Ivona <ivonaymarmat@gmail.com>
2021-10-06 23:39:43 +0200

    Neke greske u konzoli ispravljene.

--- client/src/components/searchbar/SearchBar.js
+++ client/src/components/searchbar/SearchBar.js
@@ -11,7 +11,7 @@ const SearchBar = () => {
         <button className="search-button">  
             <div className="search-icon">
                 <svg width="100%" height="100%" version="1.1" viewBox="0 0 20 20" x="0px" y="0px">
-                    <g><path fill="#18181F" fill-rule="evenodd" d="M13.192 14.606a7 7 0 111.414-1.414l3.101 3.1-1.414 1.415-3.1-3.1zM14 9A5 5 0 114 9a5 5 0 0110 0z" clip-rule="evenodd"></path></g>
+                    <g><path fill="#18181F" fillRule="evenodd" d="M13.192 14.606a7 7 0 111.414-1.414l3.101 3.1-1.414 1.415-3.1-3.1zM14 9A5 5 0 114 9a5 5 0 0110 0z" clipRule="evenodd"></path></g>
                 </svg>
             </div>
         </button>
