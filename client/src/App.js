f9634abdb6e6aa46327b30907a3c313705c0efdc
Katarina Antesevic <katarinaantesevic@gmail.com>
2021-10-07 00:22:47 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ function App() {
           <Route path="/filmovi/*" exact component={Detalji}/>
         </Switch>
         <ToastContainer hideProgressBar></ToastContainer>
+        
       </div>;
     </Router>
 }
