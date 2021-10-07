1e32974e83252b2fd82c9b6dd0765dfa2f0e9559
MilovanB <bosancicmilovan@gmail.com>
2021-10-07 06:22:56 +0200

    ..

--- client/src/components/info/MovieInfo.js
+++ client/src/components/info/MovieInfo.js
@@ -1,135 +1,234 @@
-import React, { useState, useEffect} from 'react'
-import "./MovieInfo.css"
+import React, { useState, useEffect } from "react";
+import "./MovieInfo.css";
+import AuthService from "../../services/auth";
+import axios from "axios";
+import * as constants from "../../constants/constants";
 
-import axios from 'axios';
+function MovieInfo(props) {
+  var reservation_container;
+  var login_container;
 
-function MovieInfo(props) { 
-    const [film, setFilm] = useState({});
-    const selectedSeats = [];
-    var sum = 0;
-    var id = (props.movie === undefined) ? window.sessionStorage.getItem("id") : props.movie.id;
-   
-    useEffect(()=> {
-            axios.get(`http://localhost:3001/filmovi/${id}`).then((resp) => {
-                setFilm(resp.data);
-            })
-    },[]);
+  if (JSON.parse(localStorage.getItem(constants.ACCOUNT_KEY))) {
+    reservation_container = "reservation-container-loged";
+    login_container = "login-container-loged";
+  } else {
+    reservation_container = "reservation-container";
+    login_container = "login-container";
+  }
 
-    useEffect(()=>{
-        window.sessionStorage.setItem("id",film.id);
-    },[film]);
+  const [film, setFilm] = useState({});
+  const selectedSeats = [];
+  var sum = 0;
+  var id =
+    props.movie === undefined
+      ? window.sessionStorage.getItem("id")
+      : props.movie.id;
 
-    const seatHandler = (event) => {
-        if(event.target.style.background === "rgb(3, 255, 221)"){
-             event.target.style.background="rgb(255, 255, 255)";
-             const index = selectedSeats.indexOf(event.target.innerHTML);
-            if(index > -1){
-                selectedSeats.splice(index, 1);
-                sum-=5;
-                document.getElementById("ukupno").innerHTML=sum + ".00 KM";
-            }
-         }else{
-            sum+=5;
-            event.target.style.background="rgb(3, 255, 221)";
-            selectedSeats.push(event.target.innerHTML);
-            document.getElementById("ukupno").innerHTML=sum + ".00 KM";
-            
-        }
-        document.getElementById("seats-numbers").value = selectedSeats;
-    
+  useEffect(() => {
+    axios.get(`http://localhost:3001/filmovi/${id}`).then((resp) => {
+      setFilm(resp.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    window.sessionStorage.setItem("id", film.id);
+  }, [film]);
+
+  const seatHandler = (event) => {
+    if (event.target.style.background === "rgb(3, 255, 221)") {
+      event.target.style.background = "rgb(255, 255, 255)";
+      const index = selectedSeats.indexOf(event.target.innerHTML);
+      if (index > -1) {
+        selectedSeats.splice(index, 1);
+        sum -= 5;
+        document.getElementById("ukupno").innerHTML = sum + ".00 KM";
+      }
+    } else {
+      sum += 5;
+      event.target.style.background = "rgb(3, 255, 221)";
+      selectedSeats.push(event.target.innerHTML);
+      document.getElementById("ukupno").innerHTML = sum + ".00 KM";
     }
+    document.getElementById("seats-numbers").value = selectedSeats;
+  };
 
-    return (
-        <div className="movie-reservation-container">
-            <div className="transparent-background">                
-                <div className="movie-info-container">
-                    <div className="movie-img">
-                        <img src={film.slika}/>
-                    </div>
-                    <div className="movie-text">
-                        <p>{film.orginalniNaslov}</p>
-                        <p>{film.termini}</p>
-                        <p>{film.zanr}</p>
-                        <p>{film.reziser}</p>
-                        <p>{film.glumci}</p>
-                        <p>{film.datumPremijere}</p>
-                        <p>{film.trajanjeFilma}</p>
-                        <p>{film.sadrzajFilma}</p>
-                    </div>
-                </div>
-                <div className="horizontal-fill"></div>
-                <div className="reservation-container">
-                    <div className="seats">
-                        <div className="screen">Izaberite sjediste</div>
-                    <div class="grid-container">
-                        <div class="grid-item"><button onClick={seatHandler}>1</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>2</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>3</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>4</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>5</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>6</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>7</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>8</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>9</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>10</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>11</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>12</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>13</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>14</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>15</button></div>
-                        <div class="grid-item"><button  onClick={seatHandler}>16</button></div>
-                        <div class="grid-item"><button  onClick={seatHandler}>17</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>18</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>19</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>20</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>21</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>22</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>23</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>24</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>25</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>26</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>27</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>28</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>29</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>30</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>31</button></div>                    
-                        <div class="grid-item"><button onClick={seatHandler}>32</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>33</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>34</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>35</button></div>
-                        <div class="grid-item"><button onClick={seatHandler}>36</button></div>
-                        </div>
-                    </div>
-                    <div className="reservation-form">
-                        <form action="/reservation">                          
-                                <label>Datum</label>
-                                <select id="date" name="date">
-                                    <option value="date1">4.10.2021.</option>
-                                    <option value="date1">5.10.2021.</option>
-                                    <option value="date1">6.10.2021.</option>
-                                </select>                   
-                                <label>Vrijeme</label>
-                                <select id="time" name="time">
-                                    <option value="time1">15:00</option>
-                                    <option value="time1">21:00</option>
-                                    <option value="time1">17:00</option>
-                                </select>
-                                <label>Sala</label>
-                                <select id="sala" name="sala">
-                                    <option value="sala1">sala1</option>
-                                    <option value="sala1">sala2</option>
-                                    <option value="sala1">sala3</option>
-                                </select>
-                                <label>Pozicije</label>
-                                <input type="text" id="seats-numbers" name="seat-numbers" readOnly className="input-seat" ></input>
-                                <div className="prostor" id="ukupno">0.00 KM</div>
-                                <input type="submit" value="Potvrdi" className="button-submit"/>
-                                            </form></div>
-                </div>
-            </div> 
-        
-       </div>
-    )
+  return (
+    <div className="movie-reservation-container">
+      <div className="transparent-background">
+        <div className="movie-info-container">
+          <div className="movie-img">
+            <img src={film.slika} />
+          </div>
+          <div className="movie-text">
+            <p>{film.orginalniNaslov}</p>
+            <p>{film.termini}</p>
+            <p>{film.zanr}</p>
+            <p>{film.reziser}</p>
+            <p>{film.glumci}</p>
+            <p>{film.datumPremijere}</p>
+            <p>{film.trajanjeFilma}</p>
+            <p>{film.sadrzajFilma}</p>
+          </div>
+        </div>
+        <div className="horizontal-fill"></div>
+        <div className={login_container}>
+          <label>Za rezervaciju karata potrebno je da se prijavite!</label>
+          <a className="prijava" href="/prijava">
+            Prijava
+          </a>
+        </div>
+        <div className={reservation_container}>
+          <div className="seats">
+            <div className="screen">Izaberite sjediste</div>
+            <div class="grid-container">
+              <div class="grid-item">
+                <button onClick={seatHandler}>1</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>2</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>3</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>4</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>5</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>6</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>7</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>8</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>9</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>10</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>11</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>12</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>13</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>14</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>15</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>16</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>17</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>18</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>19</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>20</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>21</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>22</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>23</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>24</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>25</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>26</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>27</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>28</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>29</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>30</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>31</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>32</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>33</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>34</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>35</button>
+              </div>
+              <div class="grid-item">
+                <button onClick={seatHandler}>36</button>
+              </div>
+            </div>
+          </div>
+          <div className="reservation-form">
+            <form action="/reservation">
+              <label>Datum</label>
+              <select id="date" name="date">
+                <option value="date1">4.10.2021.</option>
+                <option value="date1">5.10.2021.</option>
+                <option value="date1">6.10.2021.</option>
+              </select>
+              <label>Vrijeme</label>
+              <select id="time" name="time">
+                <option value="time1">15:00</option>
+                <option value="time1">21:00</option>
+                <option value="time1">17:00</option>
+              </select>
+              <label>Sala</label>
+              <select id="sala" name="sala">
+                <option value="sala1">sala1</option>
+                <option value="sala1">sala2</option>
+                <option value="sala1">sala3</option>
+              </select>
+              <label>Pozicije</label>
+              <input
+                type="text"
+                id="seats-numbers"
+                name="seat-numbers"
+                readOnly
+                className="input-seat"
+              ></input>
+              <div className="prostor" id="ukupno">
+                0.00 KM
+              </div>
+              <input type="submit" value="Potvrdi" className="button-submit" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MovieInfo
+export default MovieInfo;
---
+++ client/src/components/navbarLogged/NavbarLogged.js
@@ -0,0 +1,94 @@
+import React from "react";
+import SearchBar from "../searchbar/SearchBar";
+import "./NavbarLogged.css";
+import AuthService from "../../services/auth";
+import * as constants from "../../constants/constants";
+import axios from "axios";
+
+const obrisi = (id) => {
+  AuthService.remove(id);
+};
+
+const odjava = () => {
+  AuthService.logOut();
+};
+
+const NavbarLogged = () => {
+  const { korisnik: korisnik } = JSON.parse(
+    localStorage.getItem(constants.ACCOUNT_KEY)
+  );
+  const { korisnicko_ime: korisnicko_ime, id: id } = korisnik;
+
+  const MojStil = {
+    stroke: "rgb(167, 163, 204)",
+    fill: "rgb(100, 100, 100)",
+    strokeWidth: 8,
+    strokeDasharray: "none",
+    strokeLinecap: "butt",
+    strokeDashoffset: 0,
+    strokeLinejoin: "miter",
+    strokeMiterlimit: 4,
+    fillRule: "nonzero",
+    opacity: 1,
+  };
+
+  return (
+    <nav className="nav-container">
+      <div className="navbar">
+        <div className="logo-container">
+          <svg
+            version="1.1"
+            height="100%"
+            viewBox="0 0 303 217"
+            xmlSpace="preserve"
+          >
+            <g transform="matrix(1.9 0 0 1 113.98 155)">
+              <polygon style={MojStil} points="-50,-50 -50,50 50,50 50,-50 " />
+            </g>
+            <g transform="matrix(1.25 0 0 1.25 54 55)">
+              <circle style={MojStil} cx="0" cy="0" r="40" />
+            </g>
+            <g transform="matrix(1.25 0 0 1.25 166 54)">
+              <circle style={MojStil} cx="0" cy="0" r="40" />
+            </g>
+            <g transform="matrix(0 1.18 1.02 0 256.37 154.5)">
+              <polygon style={MojStil} points="0,-42.5 50,42.5 -50,42.5 " />
+            </g>
+          </svg>
+        </div>
+        <div className="filler1"></div>
+        <SearchBar />
+        <div className="filler2"></div>
+        <div className="link-container">
+          <ul>
+            <li>
+              <a href="/pocetna">Početna</a>
+            </li>
+            <li>
+              <a href="/sale">Sale</a>
+            </li>
+            <li>
+              <div class="dropdown">
+                <button class="dropbtn">{korisnicko_ime}</button>
+                <div class="dropdown-content">
+                  <a href="/korisnici/projenaPodataka">Izmjena podataka</a>
+                  <a href="#">Rezervacije</a>
+                  <a href="/>
+                    obrisi nalog
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <a href="/" onClick={odjava}>
+                Odjava
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavbarLogged;
